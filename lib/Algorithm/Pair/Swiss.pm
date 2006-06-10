3d3fae79c737dc72b9f6b35111a0b6c28f50cc1e
giel <giel@281f1c4b-cc15-0410-a1d2-ce6f6cf79785>
2006-06-10 11:05:47 +0000

    Almost there, just the Revision tag left
    git-svn-id: file:///Users/giel/svn/Algorithm-Pair-Swiss@8 281f1c4b-cc15-0410-a1d2-ce6f6cf79785

--- lib/Algorithm/Pair/Swiss.pm
+++ lib/Algorithm/Pair/Swiss.pm
@@ -80,7 +80,7 @@ use strict;
 use warnings;
 require 5.001;
 
-use version; our $VERSION = sprintf(q{0.1.%06d} => q{$Revision:1$} =~ /(\d+)/g);
+use version; our $VERSION = sprintf(q{0.1.%06d} => q{$Revision: 1$} =~ /(\d+)/g);
 
 use Tie::IxHash 1.21;
 
